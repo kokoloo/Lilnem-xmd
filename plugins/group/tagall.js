0d17f3d78b4b0091b750233addf467f8fdb45b57
lilnemxmdpairing <tawanachidaz78@gmail.com>
2025-08-03 12:54:56 +0200

    update

--- plugins/group/tagall.js
+++
@@ -1,5 +0,0 @@
-const handler = async (m, { conn, text, participants, isAdmins, isOwner, reply }) => {
-  if (!isAdmins && !isOwner) return reply("Command reserved for admins!")
-
-  const message = text || "📢 Attention everyone!"
-  let teks = `*📢 GENERAL ANNOUNC
